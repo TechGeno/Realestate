ed331461b26ba85d8bd14a162fda69efef0c2231
TechGeno <harshasri910@gmail.com>
2024-04-22 16:25:45 +0530

    Basic Frontend- Navbar

---
+++ api/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+
+// Running the express app
+const app= express();
+const PORT= process.env.PORT || 3000;
+// app.set('view engine', 'ejs');   // need to make the file name as ejs   
+
+app.listen(PORT, () => {
+    
+})
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,23 @@
 import React from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Profile from './pages/Profile';
+import Singup from './pages/Signup';
+import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Navbar from './components/Navbar';
 export default function App() {
-  return <h1 className='text-red-500'>Hello</h1>
+  return (
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/profile" element={<Profile />}/>
+      <Route path="/signup" element={<Singup />}/>
+      <Route path="/signin" element={<Signin />}/>
+      <Route path="/about" element={<About />}/>
+    </Routes>
+    </BrowserRouter>
+  )
   
 }
