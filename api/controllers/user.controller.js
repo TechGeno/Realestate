e40c40a00fdf2585ef617683c40638dc81f3da35
TechGeno <harshasri910@gmail.com>
2024-05-22 19:18:50 +0530

    cookiesdeleted

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -42,9 +42,10 @@ export const deleteUser = async (req,res,next) => {
     if(req.user.id != req.params.id) return next(errorHandler(401, "Unauthorized account deletion"));
     try {
         await User.findByIdAndDelete(req.params.id);
-        res.status(200).json("User Deleted!"); 
+        res.clearCookie("access_token").send();
+        res.status(200).json("User Deleted!");
     }
     catch(error){
-
+        next(error);
     }
 }
