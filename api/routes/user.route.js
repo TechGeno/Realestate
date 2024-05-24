fabdb7daa02c77dc0b5e246cfe0073f2c4963918
TechGeno <harshasri910@gmail.com>
2024-05-24 11:51:33 +0530

    Get listing api done

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { test, updateUser,deleteUser } from '../controllers/user.controller.js';
+import { test, updateUser,deleteUser, getUserList } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -9,5 +9,5 @@ const router = express.Router();
 router.get('/test',test)
 router.post('/update/:id',verifyToken, updateUser)
 router.delete('/delete/:id',verifyToken, deleteUser)
-
+router.get('/lists/:id',verifyToken, getUserList);
 export default router;
