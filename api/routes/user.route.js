9a39dd2b732ce14af711e1029aa9dfd4b1599895
TechGeno <harshasri910@gmail.com>
2024-05-25 10:37:22 +0530

    Lisiting api,-bugs

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { test, updateUser,deleteUser, getUserList } from '../controllers/user.controller.js';
+import { test, updateUser,deleteUser, getUserList, getUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -10,4 +10,6 @@ router.get('/test',test)
 router.post('/update/:id',verifyToken, updateUser)
 router.delete('/delete/:id',verifyToken, deleteUser)
 router.get('/lists/:id',verifyToken, getUserList);
+router.get('/:id', verifyToken, getUser)
+
 export default router;
